f22168dd352df5dde86e84c6c7daa5d6c7d40eab
Carsten Witt <carsten.witt@gmail.com>
2017-05-11 09:25:50 +0200

    Test empty string separators as well

--- tests/src/PathPrefixerTest.php
+++ tests/src/PathPrefixerTest.php
@@ -20,7 +20,12 @@ class PathPrefixerTest extends \PHPUnit_Framework_TestCase
     public function testUsage( $path, $separator, $expected )
     {
 
-        $sut = new PathPrefixer( $this->cwd, $separator );
+        if (!is_null($separator)) :
+            $sut = new PathPrefixer( $this->cwd, $separator );
+        else:
+            $sut = new PathPrefixer( $this->cwd );
+        endif;
+
         $result = $sut( $path );
 
         $this->assertEquals($expected, $result);
@@ -45,6 +50,7 @@ class PathPrefixerTest extends \PHPUnit_Framework_TestCase
 
         $null_separator      = null;
         $system_separator    = \DIRECTORY_SEPARATOR;
+        $empty_separator     = '';
         $alternate_separator = "@";
 
         return array(
@@ -52,15 +58,18 @@ class PathPrefixerTest extends \PHPUnit_Framework_TestCase
             [ $dir, $system_separator,    $cwd . $system_separator . $dir ],
             [ $dir, $null_separator,      $cwd . $system_separator . $dir ],
             [ $dir, $alternate_separator, $cwd . $alternate_separator . $dir ],
+            [ $dir, $empty_separator,     $cwd . $empty_separator . $dir ],
 
             // Array parameter
             [ array('foo' => $dir), $system_separator,    array('foo' => $cwd . $system_separator . $dir) ],
             [ array('foo' => $dir), $null_separator,      array('foo' => $cwd . $system_separator . $dir) ],
             [ array('foo' => $dir), $alternate_separator, array('foo' => $cwd . $alternate_separator . $dir) ],
+            [ array('foo' => $dir), $empty_separator,     array('foo' => $cwd . $empty_separator . $dir) ],
 
             // StdClass parameter
             [ (object) array('foo' => $dir), $system_separator,    (object) array('foo' => $cwd . $system_separator . $dir) ],
             [ (object) array('foo' => $dir), $null_separator,      (object) array('foo' => $cwd . $system_separator . $dir) ],
+            [ (object) array('foo' => $dir), $empty_separator,     (object) array('foo' => $cwd . $empty_separator . $dir) ],
             [ (object) array('foo' => $dir), $alternate_separator, (object) array('foo' => $cwd . $alternate_separator . $dir) ]
         );
     }
